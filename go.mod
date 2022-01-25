660302a5df7bce6702f35ef7b29b7c3cfaa34d0b
Dudás Ádám <adudas@cisco.com>
2022-01-25 12:42:19 +0100

    websocket tunnel wip

--- go.mod
+++ go.mod
@@ -4,8 +4,10 @@ go 1.16
 
 require (
 	github.com/ghodss/yaml v1.0.0
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/imdario/mergo v0.3.7 // indirect
 	github.com/spf13/cobra v0.0.5
+	github.com/stretchr/testify v1.7.0 // indirect
 	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45 // indirect
 	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
 	k8s.io/api v0.0.0-20190620084959-7cf5895f2711
---
+++ tunnel/requesthandler.go
@@ -0,0 +1,44 @@
+package tunnel
+
+import (
+	"io"
+	"net/http"
+)
+
+type RequestHandler struct {
+	RoundTripper http.RoundTripper
+}
+
+func (rh RequestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	resp, err := rh.RoundTripper.RoundTrip(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := writeResponseToResponseWriter(w, resp); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func writeResponseToResponseWriter(w http.ResponseWriter, r *http.Response) error {
+	if w == nil {
+		return nil
+	}
+	if r == nil {
+		return nil
+	}
+	// copy headers
+	// TODO: maybe some headers need to be filtered or transformed
+	for key, values := range r.Header {
+		for _, value := range values {
+			w.Header().Add(key, value)
+		}
+	}
+	// copy status code
+	w.WriteHeader(r.StatusCode)
+	// copy body
+	defer r.Body.Close()
+	_, err := io.Copy(w, r.Body)
+	return err
+}
