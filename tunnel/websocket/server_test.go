87e96897e7e30380db687f7ef1d55b43d067fe52
shifter <salovasz@cisco.com>
2022-01-26 16:37:22 +0100

    test rework

---
+++ tunnel/websocket/server_test.go
@@ -0,0 +1,26 @@
+package websocket
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewServer(t *testing.T) {
+	type args struct {
+		options []ServerOption
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Server
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewServer(tt.args.options...); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewServer() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
